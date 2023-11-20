aeaedae31265a5ff4bbad974885a3ab15e7b3074
Ole Vester <ole.vester@tum.de>
2023-11-20 21:40:34 +0100

    Add basic implementation

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import { Tooltip } from 'react-tooltip'
+import './App.css';
+import {determineEnglishWords, hasCorrectLength, hasValidCharacters} from "./password-validation-service";
+
+function App() {
+  const [password, setPassword] = useState<string>('');
+
+  return (
+      <div className="password-validation-page">
+        <h1>Password Validator @DPS</h1>
+        <div className="password-input-page">
+          <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+          />
+        </div>
+        <div className="result-container">
+            <div style={{display: "flex"}}>
+                <a></a>
+            </div>
+            <LengthCheckContainer password={password} />
+            <LetterCheckContainer password={password} />
+            <EnglishWordCheckContainer password={password} />
+        </div>
+        <p style={{color: "grey", fontSize: "small"}}>
+            Ole Vester - ole.vester@tum.de - https://github.com/ole-ve/dps-application
+        </p>
+      </div>
+  );
+}
+
+const LengthCheckContainer = ({password}: { password: string }) => {
+    const isValid = hasCorrectLength(password);
+    return <div className="validation-step-container">
+        <a className="validation-checkmark">{isValid ? '✅' : '❌'}</a>
+        <a>The password length must be between 8 and 16 characters.</a>
+    </div>;
+}
+
+const LetterCheckContainer = ({password}: { password: string }) => {
+    const isValid = hasValidCharacters(password);
+    return <div className="validation-step-container">
+        <a className="validation-checkmark">{isValid ? '✅' : '❌'}</a>
+        <a>The password must only consists of latin letters and digits and contains at least one digit.</a>
+    </div>;
+};
+
+const EnglishWordCheckContainer = ({password}: { password: string}) => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [englishWords, setEnglishWords] = useState<string[]>([]);
+
+    useEffect(() => {
+        setEnglishWords([]);
+        setIsLoading(true);
+        determineEnglishWords(password).then(words => {
+            setEnglishWords(words);
+            setIsLoading(false);
+        });
+    }, [password]);
+
+    return <div className="validation-step-container">
+
+        <a className="validation-checkmark">
+            {isLoading ? '👀' : englishWords.length === 0 ? '✅' : '❌'}
+        </a>
+        <a>The password must not contain any english words.</a>
+        {englishWords.length > 0 && <a data-tooltip-id="english-words-tooltip">ℹ</a>}
+        <Tooltip
+            id="english-words-tooltip"
+            place="bottom"
+            content={"The following word(s) are english words: " + englishWords?.join(", ")}
+        />
+    </div>;
+}
+
+export default App;
---
+++ src/password-validation-service.ts
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+/**
+ * Return if a given input string has a length between 8 and 16 characters (inclusively).
+ */
+export function hasCorrectLength(password: string): boolean {
+    return password.length >= 8 && password.length <= 16;
+}
+
+/**
+ * Return if a given input string only consists of latin letters and digits and if the input contains at least one digit.
+ */
+export function hasValidCharacters(password: string): boolean {
+    const pattern = /^(?=.*\d)[a-zA-Z0-9]+$/;
+    return pattern.test(password);
+}
+
+/**
+ * Determine all english words from a given input by extracting all words from the input and test individually.
+ */
+export async function determineEnglishWords(password: string): Promise<string[]> {
+    // Check that no full english word is contained
+    const words = extractPotentialWords(password);
+    const englishCheckResult = await Promise.all(
+        words.map(word => isEnglishWord(word).then(match => ({ word, match })))
+    );
+    return englishCheckResult.filter(entry => entry.match).map(entry => entry.word);
+}
+
+/**
+ * Extract all potential words from a given input string. In this context, a word is the longest consecutive substring
+ * of letters.
+ */
+function extractPotentialWords(input: string): string[] {
+    const substrings: string[] = [];
+    let currentSubstring = '';
+
+    for (let i = 0; i < input.length; i++) {
+        const char = input[i];
+
+        if (/[a-zA-Z]/.test(char)) {
+            currentSubstring += char;
+        } else if (currentSubstring) {
+            substrings.push(currentSubstring);
+            currentSubstring = '';
+        }
+    }
+
+    // Add the last substring if it's not empty
+    if (currentSubstring) {
+        substrings.push(currentSubstring);
+    }
+
+    return substrings;
+}
+
+const dictionaryURL = "https://api.dictionaryapi.dev/api/v2/entries/en";
+
+/**
+ * Check if a given input string is an english word by calling the free dictionary API.
+ */
+async function isEnglishWord(word: string): Promise<boolean> {
+    try {
+        await axios.get(`${dictionaryURL}/${word}`);
+        return true;
+    } catch (e: any) {
+        // The free dictionary API returns 404 if the word does not exist in their dictionary
+        return e.response?.status !== 404;
+    }
+}
